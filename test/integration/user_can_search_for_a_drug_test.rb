5e937bb97a986e5dc7c601a4a396113e8b5e00c7
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-11 14:45:35 -0700

    Add styled home page

--- test/integration/user_can_search_for_a_drug_test.rb
+++ test/integration/user_can_search_for_a_drug_test.rb
@@ -6,9 +6,8 @@ class UserCanSearchForADrugTest < ActionDispatch::IntegrationTest
 
     assert_equal "/", current_path
     assert page.has_content? "Type in the name of a prescription drug"
-    assert page.has_content? "Find out how we determine if a drug is gluten-free"
-    assert page.has_content? "api disclaimer"
-    assert page.has_link? "here"
-    assert page.has_button? "SEARCH"
+    assert page.has_link? "Find out how we determine if a drug is gluten-free"
+    assert page.has_content? "Drug information comes from the FDA, who provide it with the following disclaimer"
+    assert page.has_button? "Search"
   end
 end
