fca09944e634610227c12edd1f92f30036c27972
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-12 13:16:19 -0700

    Add test for user can search for a drug

--- test/integration/user_can_search_for_a_drug_test.rb
+++ test/integration/user_can_search_for_a_drug_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UserCanSearchForADrugTest < ActionDispatch::IntegrationTest
-  test "user sees search page as home page" do
+  test "user can search for a drug from the home page" do
     visit root_path
 
     assert_equal "/", current_path
@@ -9,5 +9,33 @@ class UserCanSearchForADrugTest < ActionDispatch::IntegrationTest
     assert page.has_link? "Find out how we determine if a drug is gluten-free"
     assert page.has_content? "Drug information comes from the FDA, who provide it with the following disclaimer"
     assert page.has_button? "Search"
+
+    fill_in "prescription", with: "tylenol"
+    click_button "Search"
+
+    assert_equal result_path, current_path
+
+    assert page.has_content?("TYLENOL is gluten-free!")
+    assert page.has_content?("Inactive ingredients:")
+    assert page.has_content?("FD&C Blue #1, FD&C Red #40, Gelatin, Polyvinylpyrrolidone, Pregelatinized Starch, Sodium Starch Glycolate, Stearic Acid, Titanium Dioxide")
+    assert page.has_content?("Active ingredients:")
+    assert page.has_content?("Acetaminophen")
+    assert page.has_content?("Ingredients with gluten:")
+    assert page.has_content?("None! :)")
+  end
+
+  test "user sees not found notice when they search for a drug with no data" do
+    skip
+    visit root_path
+
+    fill_in "prescription", with: "bee venom"
+    click_button "Search"
+
+    assert_equal result_path, current_path
+    assert page.has_content? "Sorry, the FDA has not provided ingredients for this drug :("
+
+    click_button "Try another search"
+
+    assert_equal root_path, current_path
   end
 end
