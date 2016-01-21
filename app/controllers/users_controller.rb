105f33cadd921419bedd6993503659da50724d9b
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-20 17:35:51 -0700

    Change how user show is routed
    - Previously used current_user to determine if it
    should route to '/profile' but this caused errors,
    so it now checks if :user_id is saved in the session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    if current_user
+    if session[:user_id]
       @searches = current_user.searches
     else
       redirect_to root_path
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
