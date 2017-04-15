b13c4dc9735d89063305a71f2c43bb9e582ceff3
Torie Joy-Warren <victoria.joywarren@gmail.com>
2017-04-15 14:03:26 -0700

    Drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120052238) do
+ActiveRecord::Schema.define(version: 20170415205736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,13 +35,5 @@ ActiveRecord::Schema.define(version: 20160120052238) do
 
   add_index "suspect_ingredients", ["search_id"], name: "index_suspect_ingredients_on_search_id", using: :btree
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.string   "third_party_id"
-  end
-
-  add_foreign_key "searches", "users"
   add_foreign_key "suspect_ingredients", "searches"
 end
